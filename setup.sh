2a5573dd5e33b955e6640380debb1248d6d12a37
Tom Doerr <tomdoerr96@gmail.com>
2019-09-12 15:15:23 +0200

    Improve setup deepspeech

--- setup.sh
+++ setup.sh
@@ -6,5 +6,9 @@ git clone https://github.com/mozilla/DeepSpeech.git
 cd DeepSpeech
 git checkout v0.4.1
 cd ..
-wget https://github.com/mozilla/DeepSpeech/releases/download/v0.4.1/deepspeech-0.4.1-checkpoint.tar.gz
-tar -xzf deepspeech-0.4.1-checkpoint.tar.gz
+if [ ! -d deepspeech-0.4.1-checkpoint ]
+then
+    wget https://github.com/mozilla/DeepSpeech/releases/download/v0.4.1/deepspeech-0.4.1-checkpoint.tar.gz
+    tar -xzf deepspeech-0.4.1-checkpoint.tar.gz
+    rm deepspeech-0.4.1-checkpoint.tar.gz
+fi

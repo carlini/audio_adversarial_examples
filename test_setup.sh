772ff171d36bcc25cea9b6ab3f75288e998276e3
Tom Dörr <tom.doerr@tum.de>
2020-07-05 14:37:23 +0200

    Update test

--- test_setup.sh
+++ test_setup.sh
@@ -13,12 +13,11 @@ cd audio_adversarial_examples
 docker build --no-cache -t "aae_deepspeech_041_gpu"  - < docker/aae_deepspeech_041_gpu.dockerfile
 docker build --no-cache -t "aae_deepspeech_041_cpu"  - < docker/aae_deepspeech_041_cpu.dockerfile
 ./setup.sh
-docker run --gpus all -it --mount src=$(pwd),target=/audio_adversarial_examples,type=bind -w /audio_adversarial_examples aae_deepspeech_041_gpu bash -c '
+docker run --gpus all --mount src=$(pwd),target=/audio_adversarial_examples,type=bind -w /audio_adversarial_examples aae_deepspeech_041_gpu bash -c '
 python3 classify.py --in sample-000000.wav --restore_path deepspeech-0.4.1-checkpoint/model.v0.4.1 &&\
 python3 attack.py --in sample-000000.wav --target "this is a test" --out adv.wav --iterations 1000 --restore_path deepspeech-0.4.1-checkpoint/model.v0.4.1 &&\
 python3 classify.py --in adv.wav --restore_path deepspeech-0.4.1-checkpoint/model.v0.4.1
 ' &&\
-echo Test successful! &&\
 cd .. &&\
 rm_test_dir
 
